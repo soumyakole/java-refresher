29f4971c009cb7f29f398d7be3392c5400b76358
soumyabrata kole <soumyabrata.kole@icloud.com>
2019-09-15 11:09:04 +0530

    FunctionalInterfaceTst

--- src/main/java/com/soumya/lambda/FunctionalInterfaceTst.java
+++ src/main/java/com/soumya/lambda/FunctionalInterfaceTst.java
@@ -1,5 +1,13 @@
 package com.soumya.lambda;
 
+/**
+ * A functional interface is an interface that contains only one abstract method.
+ * They can have only one functionality to exhibit.
+ * From Java 8 onwards, lambda expressions can be used to represent the instance of a functional interface.
+ * A functional interface can have any number of default methods.
+ * Runnable, ActionListener, Comparable are some of the examples of functional interfaces.
+ */
+
 @FunctionalInterface
 interface FunctionalInterfaceTst {
     int calculate(int x);
