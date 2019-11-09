40d5ea33e39f87c56bf8e5a71770a22ced778fe1
soumyabrata kole <soumyabrata.kole@icloud.com>
2019-11-10 00:34:47 +0530

    loop end condition added

--- src/main/java/com/soumya/corejava/enumdemo/EnumMain.java
+++ src/main/java/com/soumya/corejava/enumdemo/EnumMain.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class EnumMain{
     public static void main(String[] args)  {
+        boolean gotADay = false;
         System.out.print("Enter a number: ");
         Scanner input = new Scanner(System.in);
         int userInput = Integer.parseInt(input.next());
         for (WeekDays w: WeekDays.values()){
             if (userInput == w.getDayNum()){
+                gotADay = true;
                 System.out.println("Today is " + w);
                 System.out.println(w.getDaysGreeting());
                 if(w.getIsWeekend()){
                     System.out.println("Today is weekend  ...... Enjoy !!!");
+                    break;
                 }
             }
         }
-
+        if (!gotADay){
+            System.out.println("Please enter a number between 1 to 7 !!!");
+        }
     }
 }
