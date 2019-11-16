3e1401ecdc0f04dc0091e60eccfa4be1a5739e8c
soumyabrata kole <soumyabrata.kole@icloud.com>
2019-11-16 23:21:09 +0530

    initial commit

--- src/main/java/com/soumya/exceptions/AutoCloseableResourceMain.java
+++ src/main/java/com/soumya/exceptions/AutoCloseableResourceMain.java
@@ -3,7 +3,7 @@ package com.soumya.exceptions;
 public class AutoCloseableResourceMain {
     public static void main(String[] args){
         try(AutoCloseableResource aR = new AutoCloseableResource()){
-            int i = 1/0;
+            int i = 1/0; // This will raise ArithmeticException
             aR.doSomething(); // this method will never be called, but the resource will be closed.
         }catch (Exception e){
             e.printStackTrace();
